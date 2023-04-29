65c7997fc135e1e515d8aca8a69e1f931434aa91
Deák Attila <114734650+dattila5@users.noreply.github.com>
2023-04-29 14:01:03 +0200

    Interface logic Done (IShopeeLogic)

---
+++ GadgetShopeeManager/BL/IShopeeLogic.cs
@@ -0,0 +1,20 @@
+﻿using GadgetShopeeManager.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GadgetShopeeManager.BL
+{
+    internal interface IShopeeLogic
+    {
+        public ObservableCollection<Gadget> LoadData();
+        public void AddGadget();
+        public void ExportData(ObservableCollection<Gadget> gadgets);
+        public void DeleteGadget(ObservableCollection<Gadget> gadgets, Gadget SelectedGadget);
+        public void ShellGadget(Gadget SelectedGadget);
+    }
+}
